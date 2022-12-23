3606b444dfa81a5fbe7c3dfe35be951131b81a26
Danijel <susakovich@gmail.com>
2022-12-23 23:11:52 +0100

    Finish task2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function (name, num) {
+  let finalString = "";
 
+  while (num > 0) {
+    finalString += name;
+    num--;
+  }
+  return finalString;
 };
-
+repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;
