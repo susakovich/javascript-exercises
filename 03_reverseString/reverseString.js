4715d3348729398e16751d7cc36190cb86f876d8
Danijel <susakovich@gmail.com>
2022-12-24 22:06:15 +0100

    Finish exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  return str.split("").reverse().join("");
 };
-
+console.log(reverseString("Dado"));
 // Do not edit below this line
 module.exports = reverseString;
