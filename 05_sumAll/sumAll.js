337665a835b6e4ba0dbdace5a762a10dfd3b01a3
Danijel <susakovich@gmail.com>
2022-12-25 09:48:03 +0100

    Finish exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let sum = 0;
 
+  for (let i = 1; i <= num2; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
